ee5e031dc9b6120ac89d4524638ab172a8844712
Isaque Oliveira <isaque_dornelas@hotmail.com>
2022-05-25 21:41:50 -0300

    primeiro push

---
+++ Trabalho.Grpc/Trabalho.Grpc.Cliente/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Trabalho.Grpc.Servidor;
+
+namespace Trabalho.Grpc.Cliente.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ClienteController : Controller
+    {
+        public ClienteController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult ComunicacaoGrpc()
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7117");
+            return Ok();
+        }
+    }
+}
---
+++ Trabalho.Grpc/Trabalho.Grpc.Cliente/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Trabalho.Grpc.Cliente
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Cliente Grpc",
+                    Description = "Cliente Api responsável por se comunicar com um serviço Grpc. Trabalho da matéria de Sistemas Distribuidos da Unileste. Professor Thales",
+                    Contact = new OpenApiContact() { Name = "Isaque de Oliveira, Cinthia Lima Carvalho, Tulio Augusto, Denilson Vieira, Pedro", Email = "isaque.barbosa@a.unileste.edu.br" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            app.UseCors(cors =>
+                cors.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
